5824054e0747c7babfe567ec8f74e5547adb5ce4
Alex Marshall <amhokies@gmail.com>
2015-12-24 11:40:20 -0500

    Search results now display as a list in the popup

--- popup.js
+++ popup.js
@@ -6,9 +6,9 @@ document.getElementById("search-button").addEventListener("click", function() {
     searchShows(document.getElementById('search-text').value);
 });
 
-void searchShows(searchText) {
+function searchShows(searchText) {
     var query = "http://api-public.guidebox.com/v1.43/US/" + api_key +
-        "/search/title/" + encodeURIComponent(text) + "/fuzzy";
+        "/search/title/" + encodeURIComponent(searchText) + "/fuzzy";
     var request = new XMLHttpRequest();
     request.open( "GET", query, true);
     request.onreadystatechange = function () {
@@ -22,11 +22,22 @@ void searchShows(searchText) {
         var json = JSON.parse(request.responseText);
         var results = json.results;
 
-        for (i in results) {
-            console.log(results[i].title);
-        }
+        listResults(results);
     };
     request.send();
+}
 
-    document.getElementById("results").innerHTML = query;
+function listResults(results) {
+    var list = document.getElementById('results-list');
+
+    // Clear the list
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+
+    for (i in results) {
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(results[i].title));
+        list.appendChild(item);
+    }
 }
